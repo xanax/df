1aff0d344c2b9d24854d7d4807aa8194d0dd7b2f
Brendan Poole <brendan.poole@landregistry.gov.uk>
2021-03-25 19:49:55 +0000

    x

--- src/test/java/uk/co/gosseyn/xanax/domain/PointTest.java
+++ src/test/java/uk/co/gosseyn/xanax/domain/PointTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.stream.IntStream;
 
+import static java.util.stream.IntStream.range;
 import static org.junit.jupiter.api.Assertions.*;
 class PointTest {
 
@@ -16,14 +17,21 @@ class PointTest {
 
     @Test
     public void distanceToTime() {
+        // Works out about 20% faster without sqrt
         long start = System.nanoTime();
-        IntStream.range(0, 100000000).forEach(i -> {
+        range(0, 100000000).forEach(i -> {
+            randomPoint().distanceTo(randomPoint());
+        });
+        System.out.println((System.nanoTime() - start) / 1000000);
+        start = System.nanoTime();
+        range(0, 100000000).forEach(i -> {
             randomPoint().distanceToPow2(randomPoint());
         });
         System.out.println((System.nanoTime() - start) / 1000000);
     }
 
     private Point randomPoint() {
+        // TODO random here is very expensive.
         return new Point((int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100));
     }
 }
